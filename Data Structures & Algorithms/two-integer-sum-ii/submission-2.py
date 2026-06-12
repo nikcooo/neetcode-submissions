ac9ceb9066ba0618f15fd92ff653c9ecda6f9500
nikcooo <nicolaslevelingm@gmail.com>
2026-06-12 19:50:26 +0200

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        result = []
+        slowpointer = 0
+        fastpointer = len(numbers)-1
+        while target != (numbers[slowpointer]+numbers[fastpointer]):
+            if target>(numbers[slowpointer]+numbers[fastpointer]):
+                slowpointer +=1
+            else:
+                fastpointer -=1
+        result.append(slowpointer+1)
+        result.append(fastpointer+1)
+        return result
+
+
+            
+
+        
